30c6c21aab831be3f5a00c4bdc2392a2fe6cffd1
zhanpoint <167268345+zhanpoint@users.noreply.github.com>
2025-04-17 16:07:49 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,11 +4,11 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: '织梦者',
+  description: '织梦者是一个融合心理学、神经科学和生成式AI的梦境交互代理。',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'zh-Hans',
 }
 
 export const API_PREFIX = '/api'
